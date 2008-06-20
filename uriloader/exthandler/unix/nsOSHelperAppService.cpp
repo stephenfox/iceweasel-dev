e2922aeac9391e9172e4728869534d36c8910064
Mike Hommey <glandium@debian.org>
2008-06-21 00:26:07 +0200

    Ignore mailcap entries with "needsterminal"

--- uriloader/exthandler/unix/nsOSHelperAppService.cpp
+++ uriloader/exthandler/unix/nsOSHelperAppService.cpp
@@ -1161,6 +1161,9 @@ nsOSHelperAppService::GetHandlerAndDescriptionFromMailcapFile(const nsAString& a
                 }
               } else {
                 // This is an option that just has a name but no value (eg "copiousoutput")
+		if (optionName.EqualsLiteral("needsterminal")) {
+		  match = PR_FALSE;
+		}
               }
             }
             
