7dd5553d5cd0296110585b8ed83790faf2bc87ae
Mike Hommey <glandium@debian.org>
2008-07-14 11:08:51 +0200

    Import upstream version 1.9.0.1

--- client.mk
+++ client.mk
@@ -410,11 +410,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = FIREFOX_3_0_RELEASE
-NSPR_CO_TAG          = FIREFOX_3_0_RELEASE
-NSS_CO_TAG           = FIREFOX_3_0_RELEASE
-LDAPCSDK_CO_TAG      = FIREFOX_3_0_RELEASE
-LOCALES_CO_TAG       = FIREFOX_3_0_RELEASE
+MOZ_CO_TAG           = FIREFOX_3_0_1_RELEASE
+NSPR_CO_TAG          = FIREFOX_3_0_1_RELEASE
+NSS_CO_TAG           = FIREFOX_3_0_1_RELEASE
+LDAPCSDK_CO_TAG      = FIREFOX_3_0_1_RELEASE
+LOCALES_CO_TAG       = FIREFOX_3_0_1_RELEASE
 
 #######################################################################
 # Defines
