05e548fbbf3942c574f32c519291541fb3bda0d0
Mike Hommey <glandium@debian.org>
2008-09-27 17:17:39 +0200

    Don't register plugins if the MOZILLA_DISABLE_PLUGINS environment variable is set

--- modules/plugin/base/src/nsPluginHost.cpp
+++ modules/plugin/base/src/nsPluginHost.cpp
@@ -237,6 +237,24 @@ PRBool gSkipPluginSafeCalls = PR_FALSE;
 nsIFile *nsPluginHost::sPluginTempDir;
 nsPluginHost *nsPluginHost::sInst;
 
+// Globally disable plugins
+static
+int pluginsdisabled()
+{
+  static int _disabled = -1;
+
+  if (_disabled >= 0)
+    return _disabled;
+
+  const char *env = PR_GetEnv("MOZILLA_DISABLE_PLUGINS");
+  if (env && env[0])
+    _disabled = 1;
+  else
+    _disabled = 0;
+
+  return _disabled;
+}
+
 // flat file reg funcs
 static
 PRBool ReadSectionHeader(nsPluginManifestLineReader& reader, const char *token)
@@ -4872,6 +4890,10 @@ nsresult nsPluginHost::FindPlugins(PRBool aCreatePluginList, PRBool * aPluginsCh
   // possible reset in subsequent ScanPluginsDirectory calls
   PRBool pluginschanged = PR_FALSE;
 
+  if (pluginsdisabled()) {
+    mPluginsLoaded = PR_TRUE;
+    return NS_OK;
+  }
   // Scan the app-defined list of plugin dirs.
   rv = dirService->Get(NS_APP_PLUGINS_DIR_LIST, NS_GET_IID(nsISimpleEnumerator), getter_AddRefs(dirList));
   if (NS_SUCCEEDED(rv)) {
@@ -5160,6 +5182,9 @@ nsPluginHost::WritePluginInfo()
 nsresult
 nsPluginHost::ReadPluginInfo()
 {
+  if (pluginsdisabled())
+    return NS_ERROR_FAILURE;
+
   nsresult rv;
 
   nsCOMPtr<nsIProperties> directoryService(do_GetService(NS_DIRECTORY_SERVICE_CONTRACTID,&rv));
