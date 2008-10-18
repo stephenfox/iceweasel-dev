2edc0d540cf6a023db226026c5f43f47038f3e67
Mike Hommey <mh@glandium.org>
2008-10-18 14:30:06 +0200

    Remove general.useragent.locale prefs from firefox*.js
    It is better defined in the GRE provided by xulrunner-1.9

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -181,7 +181,6 @@ pref("lightweightThemes.update.enabled", true);
 pref("keyword.enabled", true);
 pref("keyword.URL", "chrome://browser-region/locale/region.properties");
 
-pref("general.useragent.locale", "@AB_CD@");
 pref("general.skins.selectedSkin", "classic/1.0");
 pref("general.useragent.extra.firefox", "@APP_UA_NAME@/@APP_VERSION@");
 
