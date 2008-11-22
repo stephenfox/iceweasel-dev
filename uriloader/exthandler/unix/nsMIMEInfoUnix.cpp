0fc7db6053ecbf7fd4c08b8c8fcd7311c263ae3b
Mike Hommey <glandium@debian.org>
2008-11-22 09:35:23 +0100

    Properly launch applications set in $HOME/.mailcap
    Replaces the previous patch in a much simpler way.

--- uriloader/exthandler/unix/nsMIMEInfoUnix.cpp
+++ uriloader/exthandler/unix/nsMIMEInfoUnix.cpp
@@ -73,6 +73,12 @@ nsMIMEInfoUnix::LoadUriInternal(nsIURI * aURI)
 NS_IMETHODIMP
 nsMIMEInfoUnix::GetHasDefaultHandler(PRBool *_retval)
 {
+  // if mDefaultApplication is set, it means the application has been set from
+  // either /etc/mailcap or ${HOME}/.mailcap, in which case we don't want to
+  // give the GNOME answer.
+  if (mDefaultApplication)
+    return nsMIMEInfoImpl::GetHasDefaultHandler(_retval);
+
   *_retval = PR_FALSE;
   nsCOMPtr<nsIGnomeVFSService> vfs = do_GetService(NS_GNOMEVFSSERVICE_CONTRACTID);
   if (vfs) {
@@ -92,14 +98,19 @@ nsMIMEInfoUnix::GetHasDefaultHandler(PRBool *_retval)
     return NS_OK;
   }
 #endif
-
-  // If we didn't find a VFS handler, fallback.
-  return nsMIMEInfoImpl::GetHasDefaultHandler(_retval);
+  
+  return NS_OK;
 }
 
 nsresult
 nsMIMEInfoUnix::LaunchDefaultWithFile(nsIFile *aFile)
 {
+  // if mDefaultApplication is set, it means the application has been set from
+  // either /etc/mailcap or ${HOME}/.mailcap, in which case we don't want to
+  // give the GNOME answer.
+  if (mDefaultApplication)
+    return nsMIMEInfoImpl::LaunchDefaultWithFile(aFile);
+
   nsCAutoString nativePath;
   aFile->GetNativePath(nativePath);
 
