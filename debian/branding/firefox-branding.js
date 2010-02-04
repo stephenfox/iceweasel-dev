a424601ef0559a583533dbbc16d1309430ef87e5
Mike Hommey <mh@glandium.org>
2010-02-04 17:49:57 +0100

    Add a release notes URL

--- debian/branding/firefox-branding.js
+++ debian/branding/firefox-branding.js
@@ -1,2 +1,3 @@
 pref("startup.homepage_override_url","http://mozilla.debian.net/%APP%/%VERSION%/whatsnew/");
 pref("startup.homepage_welcome_url","http://mozilla.debian.net/%APP%/%VERSION%/firstrun/");
+pref("app.releaseNotesURL", "http://mozilla.debian.net/%APP%/%VERSION%/releasenotes/");
