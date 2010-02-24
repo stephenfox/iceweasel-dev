a127c185918f18f4a64276a80e918c3415a7ce39
Mike Hommey <mh@glandium.org>
2010-02-24 16:25:04 +0100

    Avoid mangling some exported symbols from libmozjs

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -1429,6 +1429,7 @@ class JSAutoResolveFlags
 
 #endif /* __cplusplus */
 
+JS_BEGIN_EXTERN_C
 /*
  * Slightly more readable macros for testing per-context option settings (also
  * to hide bitset implementation detail).
@@ -1849,5 +1850,7 @@ js_RegenerateShapeForGC(JSContext *cx)
     cx->runtime->shapeGen = shape;
     return shape;
 }
+
+JS_END_EXTERN_C
 
 #endif /* jscntxt_h___ */
