d4f8aaf8d08d9e9f9b0abc3b80082b52462e79c5
Mike Hommey <glandium@debian.org>
2010-03-29 13:19:28 +0200

    Revert "Load distribution bundles from GRE directory, too"
    This reverts commit 7d30586c117700f8920338c1c7e73beb548c9583.

--- toolkit/xre/nsXREDirProvider.h
+++ toolkit/xre/nsXREDirProvider.h
@@ -137,7 +137,7 @@ protected:
   // Calculate all bundle directories, including distribution bundles,
   // extensions, and themes
   void LoadBundleDirectories();
-  void LoadAppBundleDirs(nsIFile *aBaseDir);
+  void LoadAppBundleDirs();
 
   void Append(nsIFile* aDirectory);
 
