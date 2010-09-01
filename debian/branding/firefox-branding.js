ad36f6d5f646d32f590e981a521bf3d2234f0b9f
Mike Hommey <mh@glandium.org>
2010-09-01 18:30:14 +0200

    Unset startup.homepage_override_url and startup.homepage_welcome_url
    In other words, don't display anything special after an upgrade or for the
    first time the browser is started.

--- debian/branding/firefox-branding.js
+++ debian/branding/firefox-branding.js
@@ -1,3 +1,3 @@
-pref("startup.homepage_override_url","http://mozilla.debian.net/%LOCALE%/%APP%/%VERSION%/whatsnew/");
-pref("startup.homepage_welcome_url","http://mozilla.debian.net/%LOCALE%/%APP%/%VERSION%/firstrun/");
+pref("startup.homepage_override_url","");
+pref("startup.homepage_welcome_url","");
 pref("app.releaseNotesURL", "http://mozilla.debian.net/%LOCALE%/%APP%/%VERSION%/releasenotes/");
