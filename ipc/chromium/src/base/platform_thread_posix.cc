06d046fe34a25daef042833f1bb2cd8a3225d975
Mike Hommey <mh@glandium.org>
2011-06-30 18:29:22 +0200

    Allow ipc code to build on GNU/kfreebsd

--- ipc/chromium/src/base/platform_thread_posix.cc
+++ ipc/chromium/src/base/platform_thread_posix.cc
@@ -37,7 +37,11 @@ PlatformThreadId PlatformThread::CurrentId() {
   // TODO(BSD): find a better thread ID
   return (intptr_t)(pthread_self());
 #elif defined(OS_LINUX)
+#ifdef __NR_gettid
   return syscall(__NR_gettid);
+#else
+  return getpid();
+#endif
 #endif
 }
 
