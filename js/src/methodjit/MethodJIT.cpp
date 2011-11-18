df79b7ffee2f1a48b9f3d97360ae1956f4b0d56b
Mike Hommey <mh@glandium.org>
2011-11-18 11:27:29 +0100

    Bug 698923 - Don't require 16 bytes alignment for VMFrame on sparc

--- js/src/methodjit/MethodJIT.cpp
+++ js/src/methodjit/MethodJIT.cpp
@@ -257,7 +257,7 @@ JS_STATIC_ASSERT(offsetof(FrameRegs, sp) == 0);
 #if defined(__GNUC__) && !defined(_WIN64)
 
 /* If this assert fails, you need to realign VMFrame to 16 bytes. */
-#ifdef JS_CPU_ARM
+#if defined(JS_CPU_ARM) || defined(JS_CPU_SPARC)
 JS_STATIC_ASSERT(sizeof(VMFrame) % 8 == 0);
 #else
 JS_STATIC_ASSERT(sizeof(VMFrame) % 16 == 0);
